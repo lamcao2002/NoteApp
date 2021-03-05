cb475a6185786e0ea499ec433d5268f9e6e61b72
lamcao2002 <caovohoanglam2002@gmail.com>
2021-03-05 18:25:00 +0700

    create func saveNote() in CreateNoteActivity.java

--- app/src/main/java/com/eoayem/noteapp/activities/CreateNoteActivity.java
+++ app/src/main/java/com/eoayem/noteapp/activities/CreateNoteActivity.java
@@ -2,13 +2,19 @@ package com.eoayem.noteapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.eoayem.noteapp.R;
+import com.eoayem.noteapp.database.NotesDatabase;
+import com.eoayem.noteapp.entities.Note;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -41,5 +47,53 @@ public class CreateNoteActivity extends AppCompatActivity {
                 new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
                 .format(new Date())
         );
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+            }
+        });
+    }
+
+    private void saveNote(){
+        //Validations
+        if(inputNoteTitle.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note title can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        } else if(inputNoteSubtitle.getText().toString().trim().isEmpty()
+                && inputNoteText.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //preparing note object to save in database
+        Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString());
+        note.setSubtitle(inputNoteSubtitle.getText().toString());
+        note.setNoteText(inputNoteText.getText().toString());
+        note.setDatetime(textDateTime.getText().toString());
+
+        //Room doen't allow database operation on the Main thread. Using ASYNC TASK to save note
+
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void>{
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NotesDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+
+        new SaveNoteTask().execute();
     }
 }
