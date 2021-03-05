da2a8719c0903f95064d7b5331caa197ebcf76f3
lamcao2002 <caovohoanglam2002@gmail.com>
2021-03-05 15:28:08 +0700

    create package activities, entities

---
+++ app/src/main/java/com/eoayem/noteapp/activities/MainActivity.java
@@ -0,0 +1,32 @@
+package com.eoayem.noteapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.eoayem.noteapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
+        imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        new Intent(getApplicationContext(), CreateNoteActivity.class),
+                        REQUEST_CODE_ADD_NOTE
+                );
+            }
+        });
+    }
+}
